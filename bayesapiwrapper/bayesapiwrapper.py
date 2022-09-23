8ddd2b3754fc858a894a8266da1f6222a8522e24
Santiago Kozak <santikzk1406@gmail.com>
2022-09-22 23:51:26 -0300

    Multiple modifications
    Endpoint as class constant, get_game_summary and timeline in separate functions

--- bayesapiwrapper/bayesapiwrapper.py
+++ bayesapiwrapper/bayesapiwrapper.py
@@ -4,14 +4,11 @@ from datetime import datetime, timedelta
 import os
 
 
-class NotFoundError(Exception):
-    pass
-
-
 class BayesApiWrapper(object):
     config_path = os.path.join(os.path.expanduser('~'), '.config', 'bayesapiwrapper')
     tokens_file = os.path.join(config_path, "tokens.json")
     credentials_file = os.path.join(config_path, "credentials.json")
+    endpoint = "https://emh-api.bayesesports.com/"
 
     def __init__(self):
         self.tokens = None
@@ -37,13 +34,18 @@ class BayesApiWrapper(object):
         with open(file=self.tokens_file, mode="w+", encoding="utf8") as f:
             json.dump(self.tokens, f, ensure_ascii=False)
 
+    def get_game_summary(self, game_rpgid):
+        summary_url = self.do_api_call("GET", f"api/v1/games/{game_rpgid}/download?type=GAMH_SUMMARY")["url"]
+        return requests.get(summary_url).json()
+
+    def get_game_details(self, game_rpgid):
+        details_url = self.do_api_call("GET", f"api/v1/games/{game_rpgid}/download?type=GAMH_DETAILS")["url"]
+        return requests.get(details_url).json()
+
     def get_game(self, game_rpgid):
-        self.ensure_login()
-        data_url = self.do_api_call("GET", f"api/v1/games/{game_rpgid}/download?type=GAMH_SUMMARY")["url"]
-        timeline_url = self.do_api_call("GET", f"api/v1/games/{game_rpgid}/download?type=GAMH_DETAILS")["url"]
-        data = requests.get(data_url).json()
-        timeline = requests.get(timeline_url).json()
-        return data, timeline
+        summary = self.get_game_summary(game_rpgid)
+        details = self.get_game_details(game_rpgid)
+        return summary, details
 
     def should_refresh(self):
         expires = datetime.fromtimestamp(self.tokens["expires"])
@@ -52,7 +54,8 @@ class BayesApiWrapper(object):
         return False
 
     def ensure_login(self):
-        self.load_tokens()
+        if self.tokens is None:
+            self.load_tokens()
         if "accessToken" not in self.tokens:
             self.do_login()
         if self.should_refresh():
@@ -65,7 +68,6 @@ class BayesApiWrapper(object):
         self.load_credentials()
         data = self.do_api_call("POST", "login", {"username": self.credentials["username"],
                                                   "password": self.credentials["password"]})
-        print(data)
         self.tokens = {"accessToken": data["accessToken"], "refreshToken": data["refreshToken"],
                        "expires": datetime.now().timestamp() + data["expiresIn"]}
         self.save_tokens()
@@ -76,20 +78,18 @@ class BayesApiWrapper(object):
         return {"Authorization": f"Bearer {self.tokens['accessToken']}"}
 
     def do_api_call(self, method, service, data=None):
-        endpoint = "https://emh-api.bayesesports.com/"
+        self.ensure_login()
 
         if method == "GET":
-            response = requests.get(endpoint + service, headers=self.get_headers(), params=data)
+            response = requests.get(self.endpoint + service, headers=self.get_headers(), params=data)
         elif method == "POST":
-            response = requests.post(endpoint + service, json=data)
+            response = requests.post(self.endpoint + service, json=data)
         else:
             raise ValueError("HTTP Method must be GET or POST.")
-        if response.status_code == 429:
-            raise Exception("Bayes Ratelimit! Please try again later")
-        elif response.status_code == 404:
-            raise NotFoundError()
+        if response.status_code != 200:
+            response.raise_for_status()
         return response.json()
 
 
 if __name__ == "__main__":
-    print(BayesApiWrapper().get_game("ESPORTSTMNT06_2570477"))
+    print(BayesApiWrapper().get_game_details("ESPORTSTMNT06_2580511"))
