24fc0d4c7d83df8e09640af2d6d66b4287a55192
Santiago Kozak <santikzk1406@gmail.com>
2022-11-25 14:51:40 -0300

    black formatting and use backoff

--- bayesapiwrapper/bayesapiwrapper.py
+++ bayesapiwrapper/bayesapiwrapper.py
@@ -1,17 +1,30 @@
 import requests
+from requests.exceptions import (
+    JSONDecodeError,
+    ConnectionError,
+    ConnectTimeout,
+    Timeout,
+    RequestException,
+    ReadTimeout,
+)
 import json
 from datetime import datetime, timedelta
 import os
 from typing import Optional, Union
 import pytz
+import backoff
 
 
 class NotFoundError(Exception):
     pass
 
 
+class TooManyRequests(Exception):
+    pass
+
+
 class BayesApiWrapper(object):
-    config_path = os.path.join(os.path.expanduser('~'), '.config', 'bayesapiwrapper')
+    config_path = os.path.join(os.path.expanduser("~"), ".config", "bayesapiwrapper")
     tokens_file = os.path.join(config_path, "tokens.json")
     credentials_file = os.path.join(config_path, "credentials.json")
     endpoint = "https://lolesports-api.bayesesports.com/"
@@ -26,7 +39,9 @@ class BayesApiWrapper(object):
             email = input("Bayes username: ")
             password = input("Bayes password: ")
             with open(file=self.credentials_file, mode="w+", encoding="utf8") as f:
-                json.dump({"username": email, "password": password}, f, ensure_ascii=False)
+                json.dump(
+                    {"username": email, "password": password}, f, ensure_ascii=False
+                )
         with open(file=self.credentials_file, mode="r+", encoding="utf8") as f:
             self.credentials = json.load(f)
 
@@ -40,9 +55,11 @@ class BayesApiWrapper(object):
             self.tokens = json.load(f)
 
     def _store_tokens(self, data):
-        self.tokens = {"accessToken": data["accessToken"],
-                       "refreshToken": data["refreshToken"],
-                       "expires": datetime.now().timestamp() + data["expiresIn"]}
+        self.tokens = {
+            "accessToken": data["accessToken"],
+            "refreshToken": data["refreshToken"],
+            "expires": datetime.now().timestamp() + data["expiresIn"],
+        }
         with open(file=self.tokens_file, mode="w+", encoding="utf8") as f:
             json.dump(self.tokens, f, ensure_ascii=False)
 
@@ -63,31 +80,54 @@ class BayesApiWrapper(object):
     def _refresh_token(self):
         # This ugly try/except is due to bayes returning 429 when trying to refresh token
         try:
-            self._load_tokens()
-            data = self._do_api_call("POST", "auth/refresh", {"refreshToken": self.tokens["refreshToken"]},
-                                     ensure_login=False)
+            if self.tokens is None:
+                self._load_tokens()
+            data = self._do_api_call(
+                "POST",
+                "auth/refresh",
+                {"refreshToken": self.tokens["refreshToken"]},
+                ensure_login=False,
+            )
             self._store_tokens(data)
         except requests.HTTPError:
             self._do_login()
 
     def _do_login(self):
         self._load_credentials()
-        data = self._do_api_call("POST", "auth/login", {"username": self.credentials["username"],
-                                                        "password": self.credentials["password"]},
-                                 ensure_login=False)
+        data = self._do_api_call(
+            "POST",
+            "auth/login",
+            {
+                "username": self.credentials["username"],
+                "password": self.credentials["password"],
+            },
+            ensure_login=False,
+        )
         self._store_tokens(data)
 
     def get_game_summary(self, game_rpgid):
-        summary = self.get_game_asset(game_rpgid, "GAMH_SUMMARY")
-        return summary.json()
+        return self.get_game_asset(game_rpgid, "GAMH_SUMMARY")
 
     def get_game_details(self, game_rpgid):
-        details = self.get_game_asset(game_rpgid, "GAMH_DETAILS")
-        return details.json()
+        return self.get_game_asset(game_rpgid, "GAMH_DETAILS")
 
+    @backoff.on_exception(
+        backoff.expo,
+        (
+            JSONDecodeError,
+            ConnectionError,
+            ConnectTimeout,
+            Timeout,
+            RequestException,
+            ReadTimeout,
+        ),
+        max_time=60,
+    )
     def get_game_asset(self, game_rpgid, asset_name):
-        asset_url = self._do_api_call(f"GET", f"emh/v1/games/{game_rpgid}/download", data={"type": asset_name})["url"]
-        return requests.get(asset_url)
+        asset_url = self._do_api_call(
+            f"GET", f"emh/v1/games/{game_rpgid}/download", data={"type": asset_name}
+        )["url"]
+        return requests.get(asset_url).json()
 
     def get_game(self, game_rpgid):
         summary = self.get_game_summary(game_rpgid)
@@ -106,32 +146,61 @@ class BayesApiWrapper(object):
             date = date.isoformat()
         return date
 
-    def get_game_list(self, *, tags: Optional[Union[str, list]] = None,
-                      from_timestamp: Optional[Union[datetime, str, int, float]] = None,
-                      to_timestamp: Optional[Union[datetime, str, int, float]] = None, page: Optional[int] = None,
-                      size: Optional[int] = None, team1: Optional[str] = None, team2: Optional[str] = None):
+    def get_game_list(
+        self,
+        *,
+        tags: Optional[Union[str, list]] = None,
+        from_timestamp: Optional[Union[datetime, str, int, float]] = None,
+        to_timestamp: Optional[Union[datetime, str, int, float]] = None,
+        page: Optional[int] = None,
+        size: Optional[int] = None,
+        team1: Optional[str] = None,
+        team2: Optional[str] = None,
+    ):
         if type(tags) == list:
             tags = ",".join(tags)
-        from_timestamp, to_timestamp = self._process_datetime(from_timestamp), self._process_datetime(to_timestamp)
-        params = {"from": from_timestamp, "to": to_timestamp, "tags": tags, "page": page,
-                  "size": size, "team1": team1, "team2": team2}
+        from_timestamp, to_timestamp = self._process_datetime(
+            from_timestamp
+        ), self._process_datetime(to_timestamp)
+        params = {
+            "from": from_timestamp,
+            "to": to_timestamp,
+            "tags": tags,
+            "page": page,
+            "size": size,
+            "team1": team1,
+            "team2": team2,
+        }
         game_list = self._do_api_call("GET", "emh/v1/games", params)
         return game_list
 
     def _get_headers(self):
         return {"Authorization": f"Bearer {self.tokens['accessToken']}"}
 
-    def _do_api_call(self, method, service, data=None, *, allow_retry: bool = True, ensure_login: bool = True):
+    @backoff.on_exception(backoff.expo, TooManyRequests, max_time=60)
+    def _do_api_call(
+        self,
+        method,
+        service,
+        data=None,
+        *,
+        allow_retry: bool = True,
+        ensure_login: bool = True,
+    ):
         if ensure_login:
             self._ensure_login()
         if method == "GET":
-            response = requests.get(self.endpoint + service, headers=self._get_headers(), params=data)
+            response = requests.get(
+                self.endpoint + service, headers=self._get_headers(), params=data
+            )
         elif method == "POST":
             response = requests.post(self.endpoint + service, json=data)
         else:
             raise ValueError("HTTP Method must be GET or POST.")
         if response.status_code == 401 and allow_retry:
             return self._do_api_call(method, service, data, allow_retry=False)
+        elif response.status_code == 429:
+            raise TooManyRequests
         elif response.status_code == 404:
             raise NotFoundError
         response.raise_for_status()
